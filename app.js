951521460ed2e206e7a4680d462633976e69f328
Zarif Karim <zarifkarim09@gmail.com>
2022-10-21 10:16:12 +1100

    minimal working code for client and server

---
+++ app.js
@@ -0,0 +1,23 @@
+const PORT = 5000;
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
+
+//serving public files
+app.use(express.static(__dirname+'/public'));
+
+app.get('/',(req,res)=>{
+    console.log(req.url);
+    // res.sendFile(__dirname+'/public/index.html');
+    res.end();
+});
+
+io.on('connection', (socket)=>{
+    console.log('a user connected');
+});
+
+server.listen(PORT, ()=>console.log(`Server started: http://localhost:${PORT}`));
